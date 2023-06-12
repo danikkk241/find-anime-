e7cdd268f7e7cff89656d9a54758a81cf76fba2f
danikkk241 <121660553+danikkk241@users.noreply.github.com>
2023-06-12 22:08:34 +0545

    add  function to display anime details in the modal

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
-const API_URL = 'https://kitsu.io/api/edge/anime';
-let page = 1;
+const API_URL = "https://kitsu.io/api/edge/anime";
 
-const mainContent = document.getElementById('anime-content');
-const form = document.getElementById('search-form');
-const search = document.getElementById('search');
-const pageLinks = document.querySelectorAll('.page-link');
+const mainContent = document.getElementById("anime-content");
+const modal = document.getElementById("modal");
+const modalContent = document.getElementById("modal-body");
 
-// Initially get the anime list's first page
-getAnime(API_URL, page);
-
-// Previous and next page
-pageLinks.forEach((pageLink) => {
-  pageLink.addEventListener('click', () => {
-    if (pageLink.id === 'next') {
-      page++;
-      getAnime(API_URL, page);
-    }
-    if (pageLink.id === 'previous' && page > 1) {
-      page--;
-      getAnime(API_URL, page);
-    }
+// Fetch popular anime list
+fetch(`${API_URL}?sort=-averageRating&page[limit]=20`)
+  .then((response) => response.json())
+  .then((data) => {
+    const animeList = data.data;
+    showAnimeList(animeList);
+  })
+  .catch((error) => {
+    console.error("Error fetching anime list:", error);
   });
-});
 
-// Search for an anime
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const query = search.value;
-  if (query) {
-    const searchURL = `${API_URL}?filter[text]=${query}`;
-    getAnime(searchURL, page);
-  }
-});
-
-async function getAnime(url, page) {
-  const response = await fetch(`${url}?page[limit]=20&page[offset]=${(page - 1) * 20}`);
-  const responseData = await response.json();
-  showAnime(responseData.data);
-}
-
-function showAnime(animeData) {
-  mainContent.innerHTML = '';
-  animeData.forEach((anime) => {
+// Function to display anime list
+function showAnimeList(animeList) {
+  mainContent.innerHTML = "";
+  animeList.forEach((anime) => {
+    const animeId = anime.id;
     const animeTitle = anime.attributes.canonicalTitle;
-    const animePoster = anime.attributes.posterImage ? anime.attributes.posterImage.original : 'https://via.placeholder.com/300';
-    const animeGenres = anime.attributes.genres ? anime.attributes.genres.join(', ') : 'Unknown';
-    const animeDescription = anime.attributes.description ? anime.attributes.description : 'No description available.';
-    const animeElm = document.createElement('div');
-    animeElm.classList.add('col-anime', 'p-0');
-    animeElm.innerHTML = `
+    const animePoster = anime.attributes.posterImage.small;
+
+    const animeCard = document.createElement("div");
+    animeCard.classList.add("col-xs-12", "col-sm-6", "col-md-4", "col-lg-3", "p-0");
+    animeCard.innerHTML = `
       <div class="anime-card">
-        <img class="img-fluid anime-img" src="${animePoster}" alt="${animeTitle}">
-        <div class="anime-description p-3">
+        <img class="img-fluid anime-img" src="${animePoster}" alt="${animeTitle}" data-anime-id="${animeId}">
+        <div class="anime-description p-3 d-flex justify-content-between align-items-center">
           <h3 class="anime-title">${animeTitle}</h3>
-          <p class="anime-genres"><strong>Genres:</strong> ${animeGenres}</p>
-          <p class="anime-desc"><strong>Description:</strong> ${animeDescription}</p>
         </div>
       </div>
     `;
-    mainContent.appendChild(animeElm);
+    mainContent.appendChild(animeCard);
   });
 }
+
+// Function to display anime details in the modal
+function showAnimeDetails(animeId) {
+  // Fetch anime details from Kitsu API
+  fetch(`${API_URL}/${animeId}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const anime = data.data;
+      const animeTitle = anime.attributes.canonicalTitle;
+      const animeSynopsis = anime.attributes.synopsis;
+
+      // Update modal content with anime details
+      modalContent.innerHTML = `
+        <span class="close" onclick="closeModal()">&times;</span>
+        <h2>${animeTitle}</h2>
+        <p>${animeSynopsis}</p>
+      `;
+
+      // Show modal
+      modal.style.display = "block";
+    })
+    .catch((error) => {
+      console.error("Error fetching anime details:", error);
+    });
+}
+
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Add event listener to anime images
+mainContent.addEventListener("click", (event) => {
+  const clickedElement = event.target;
+  if (clickedElement.classList.contains("anime-img")) {
+    const animeId = clickedElement.dataset.animeId;
+    showAnimeDetails(animeId);
+  }
+});
